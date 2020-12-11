bda9fa5388049cb0e9128a5c012ed5d7ca30934b
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-12-12 00:55:24 +0900

    Update plottercar.ts

--- plottercar.ts
+++ plottercar.ts
@@ -252,7 +252,7 @@ namespace eureka_plotter_car {
   //% block="左回り　角度 |%L_degree| " group="3　基本の動き"
 
   export function plottercar_L_cycle(L_degree: number): void {
-    for (let index = 0; index < (L_degree / 360) * 512 * 1.61*cond_degree; index++) {
+    for (let index = 0; index < (L_degree / 360) * 512 * 1.65*cond_degree; index++) {
     led.enable(false);
 
       let i = 0;
@@ -371,7 +371,7 @@ namespace eureka_plotter_car {
 
     let i = 0;
 
-    for (let index = 0; index < (R_degree / 360) * 512 * 1.61*cond_degree; index++) {
+    for (let index = 0; index < (R_degree / 360) * 512 * 1.65*cond_degree; index++) {
       for (i = 0; i < 1000; i++);
       {
       }
