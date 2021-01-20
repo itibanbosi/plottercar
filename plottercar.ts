c484e4d53297b3e5baa29d6781fa79673fa4be5e
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-01-20 21:27:02 +0900

    Update plottercar.ts

--- plottercar.ts
+++ plottercar.ts
@@ -768,6 +768,69 @@ namespace eureka_plotter_car {
         eureka_plotter_car.plottercar_forward(Foward_D)
         eureka_plotter_car.plottercar_R_cycle(360 / 60)
     }
+
+ //% color="#3943c6" weight=30　blockId=plottercar_forward
+  //% block="1相励起　前へ |%F_cm| ｃｍ進む" group="3　基本の動き"
+
+  export function plottercar_1sou_forward(F_cm: number): void {
+    led.enable(false);
+    let i=0;
+    for (let index = 0; index < (F_cm / (18.9*cond_Distance) * 512/4); index++) {
+
+
+      pins.digitalWritePin(DigitalPin.P3, 0);
+      pins.digitalWritePin(DigitalPin.P13, 1);
+      pins.digitalWritePin(DigitalPin.P4, 0);
+      pins.digitalWritePin(DigitalPin.P14, 0);
+      pins.digitalWritePin(DigitalPin.P6, 0);
+      pins.digitalWritePin(DigitalPin.P15, 0);
+      pins.digitalWritePin(DigitalPin.P7, 1);
+      pins.digitalWritePin(DigitalPin.P16, 0);
+
+      for (i = 0; i < 1000; i++);
+      {
+      }
+      pins.digitalWritePin(DigitalPin.P3, 0);
+      pins.digitalWritePin(DigitalPin.P13, 0);
+      pins.digitalWritePin(DigitalPin.P4, 0);
+      pins.digitalWritePin(DigitalPin.P14, 1);
+      pins.digitalWritePin(DigitalPin.P6, 1);
+      pins.digitalWritePin(DigitalPin.P15, 0);
+      pins.digitalWritePin(DigitalPin.P7, 0);
+      pins.digitalWritePin(DigitalPin.P16, 0);
+      for (i = 0; i < 1000; i++);
+      {
+      }
+      pins.digitalWritePin(DigitalPin.P3, 0);
+      pins.digitalWritePin(DigitalPin.P13, 0);
+      pins.digitalWritePin(DigitalPin.P4, 1);
+      pins.digitalWritePin(DigitalPin.P14, 0);
+      pins.digitalWritePin(DigitalPin.P6, 0);
+      pins.digitalWritePin(DigitalPin.P15, 1);
+      pins.digitalWritePin(DigitalPin.P7, 0);
+      pins.digitalWritePin(DigitalPin.P16, 0);
+      for (i = 0; i < 1000; i++);
+      {
+      }
+      pins.digitalWritePin(DigitalPin.P3, 1);
+      pins.digitalWritePin(DigitalPin.P13, 0);
+      pins.digitalWritePin(DigitalPin.P4, 0);
+      pins.digitalWritePin(DigitalPin.P14, 0);
+      pins.digitalWritePin(DigitalPin.P6, 0);
+      pins.digitalWritePin(DigitalPin.P15, 0);
+      pins.digitalWritePin(DigitalPin.P7, 0);
+      pins.digitalWritePin(DigitalPin.P16, 1);
+      for (i = 0; i < 1000; i++);
+      {
+      }
+
+    }
+  }
+
+
+
+
+
 }
 
 
